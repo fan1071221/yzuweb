c12479afff84f772b78e7f7cf38fdb565e14939b
unknown <zxvbnm456@gmail.com>
2020-10-10 12:42:37 +0800

    'yzuweb'

---
+++ BootstrapCarousel/BootstrapCarousel/Youtube心情點唱機/playlist.js
@@ -0,0 +1,12 @@
+//YouTube影片ID
+let playList = [
+    "J6p9_iDKS7Y", //讓我留在你身邊
+    "EJud0U7m1C8", //全放空
+    "ENHKBmUMxJ0" //飄向北方
+];
+//播放起訖秒數
+let playTime = [
+    [57,60],
+    [79,83],
+    [76,80]  
+]
